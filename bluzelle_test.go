46706898c48d947bc04e92fdf3234c0a344aad14
wlwanpan <wlwanpan@uwaterloo.ca>
2019-04-09 21:09:56 -0700

    missed test files

---
+++ bluzelle_test.go
@@ -0,0 +1,31 @@
+package bluzelle
+
+import (
+	"testing"
+)
+
+const (
+	TestEndPoint string = "bernoulli.bluzelle.com:51010"
+	TestUUID     string = "5f493479–2447–47g6–1c36-efa5d251a25"
+	TestPemfile  string = "./test.pem"
+)
+
+var blzTest *Bluzelle
+
+func TestConnect(t *testing.T) {
+
+	pemBytes, err := ReadPemFile(TestPemfile)
+	if err != nil {
+		t.Error(err)
+	}
+	blzTest, err = Connect(TestEndPoint, TestUUID, pemBytes)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSize(t *testing.T) {
+	if err := blzTest.Size(); err != nil {
+		t.Error(err)
+	}
+}
